bb2e3c693f10bdbc9960369f5fb66f9bf7ac1054
Justomoriconi <justomoriconi@gamil.com>
2026-06-08 15:58:15 -0300

    feat: agrega configuracion inicial de gin

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("¡Hola Mundo desde Go! Servidor Ticketek listo.")
+	r := gin.Default()
+
+	r.Run(":8080")
 }
